f2deebed0b0c4107d0daa1c7c718bce43c49032e
hadi6jokhadar <hadi6jokhadar@hotmail.com>
2020-06-25 21:04:25 +0300

    HTML + CSS in Main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MainComponent } from './main/main.component';
 import { YoutubeService } from './service/youtube.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [AppComponent, MainComponent],
-  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatInputModule,
+  ],
   providers: [YoutubeService],
   bootstrap: [AppComponent],
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,7 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.sass'],
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit, OnDestroy {
   videos: any[];
