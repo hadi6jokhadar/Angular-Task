91e9fda26ce0a2ef13df712528c8e83ea3909846
hadi6jokhadar <hadi6jokhadar@hotmail.com>
2020-06-25 20:38:29 +0300

    Setup + add Material + fxflex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MainComponent } from './main/main.component';
+import { YoutubeService } from './service/youtube.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MainComponent],
+  imports: [BrowserModule, AppRoutingModule, FlexLayoutModule, BrowserAnimationsModule],
+  providers: [YoutubeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
